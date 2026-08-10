ca35b616f82288ccb4671dec99548430efe9fee8
nikita-lol1 <st23651@howick.school.nz>
2026-08-11 10:51:34 +1200

    Add files via upload
    completed 2 functions - improved errors and both functions can run well and efficiently. will start on more personal details next time

--- PIMS_ASSESSMENT.py
+++ PIMS_ASSESSMENT.py
@@ -16,7 +16,7 @@ def get_driver_name():
         parts = user_input.split()
 
         # Ensure at least 2 words are entered and all parts contain only letters
-        if len(parts) >= 2 and parts.isalpha():
+        if len(parts) >= 2 and all(part.isalpha() for part in parts):
             driver_name = user_input.title()
             is_valid = True
         else:
@@ -24,9 +24,29 @@ def get_driver_name():
                   "Blank or single-word entries are not accepted.\n")
 
     return driver_name
+
+def get_drivers_license():
+    '''ASKS FOR LICENSE NO. - Ensures only contains 2 letters followed by 2
+    numbers in capital'''
     
-print(get_driver_name())
+    is_valid = False
+    license_no = ""
+    
+    while not is_valid: # Asks for license no. 
+        user_input = input("Enter driver's license number: ").strip().upper()
+         
+         # Ensures exactly 6 characters: first 2 are letters, last 4 are digits
+        if len(user_input) == 6 and user_input[:2].isalpha() and user_input[2:].isdigit():
+            license_no = user_input
+            is_valid = True
+        
+        else:
+            print("Error: Must be 6 characters long (2 letters followed by 4 numbers).")
+
+    return license_no  
+
+print(get_drivers_license())
+
     
 def speeding_offence():
     '''RECORDING AN OFFENCE AND COLLECTS DRIVERS RELEVANT INFO'''
-    
